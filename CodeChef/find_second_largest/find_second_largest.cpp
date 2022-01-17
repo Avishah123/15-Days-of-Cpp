a8e3d795ddf87bb0ee8f7c62567158544ec695dd
Avi Shah <getavi4@gmail.com>
2022-01-17 11:08:09 +0530

    commit 1

---
+++ CodeChef/find_second_largest/find_second_largest.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<list>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n=0;
+    int a=0,b=0,c=0;
+    
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    if(b > a && b < c && c > a )
+    {
+        cout << b;
+    }
+    else if(a > b && a < c && c > b)
+    {
+        cout << a;
+    }
+    else 
+    {
+        cout << c;
+    }
+
+    
+
+	return 0;
+}
